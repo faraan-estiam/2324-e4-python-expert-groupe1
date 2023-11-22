782ada2813067e45d1b016fdde6a3a26d2e45fbb
faraan-estiam <147595360+faraan-estiam@users.noreply.github.com>
2023-11-22 20:16:14 +0100

    removed useless spaces

--- TP01.py
+++ TP01.py
@@ -1,5 +1,7 @@
-word = 'mississippi'
 # 1. Remplacez l'une des lettre qui apparait avec la grande plus fréquence par la lettre e dans le mot m. Remplacez également les autres lettres qui apparaissent avec la même fréquence max.
-rplce = lambda w : ''.join(['e' if l in (lambda c : list(filter(lambda i: False not in [c[i] >= n for n in c.values()], c)))({k:w.count(k) for k in set(w)}) else l for l in w])
+q1=lambda w:''.join(['e'if l in(lambda c:list(filter(lambda i:False not in [c[i]>=n for n in c.values()],c)))({k:w.count(k)for k in set(w)})else l for l in w])
 # 2. Calculez la fréquence d'apparition de chaque lettre dans le mot m
-question2 = lambda w: {k:w.count(k) for k in set(w)}
+q2=lambda w:{k:w.count(k)for k in set(w)}
+
+word='mississippi'
+print(q1(word))
